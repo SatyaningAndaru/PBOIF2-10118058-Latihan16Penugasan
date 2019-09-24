7fbb2726453f08f0732a9b9e719357b2df0349d5
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 18:47:05 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan16penugasan/PBOIF210118058Latihan16Penugasan.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan16penugasan;
+
+/**
+ *
+ * @author user
+ * * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=20;
+        int c=0;
+        
+        System.out.println("nilai a = " + a);
+        
+        System.out.println("nilai b = " + b);
+        System.out.println("nilai c = " + c+"\n");
+        
+        c = a+b;
+        System.out.println(" c = a+b =" +c);
+        c += a;
+        System.out.println(" c += a =" +c);
+        c -= a;
+        System.out.println(" c -= a =" +c);
+        c *= a;
+        System.out.println(" c *= a =" +c);
+        
+         a=10;
+         c=15;
+         c/=a;
+        
+        System.out.println("nilai c /= a = " + a);
+                
+    }
+    
+}
